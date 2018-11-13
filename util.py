4b4999972cf5c9de5265dc56210a1390cda9e2d8
edithzeng <edith.xiaoyu.zeng@utexas.edu>
2018-11-13 02:08:15 +0000

    add deep feature extract

--- util.py
+++ util.py
@@ -12,8 +12,6 @@ from skimage import io
 import cv2
 import matplotlib.pyplot as plt
 import json
-from sklearn.feature_extraction.text import CountVectorizer
-import nltk
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
@@ -25,6 +23,13 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import VotingClassifier
 from sklearn.ensemble import RandomForestClassifier
+import keras
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
 
 def main():
     pass
@@ -49,24 +54,62 @@ def aggregate_visual_features(df_train, df_train_features, cv_key):
         # get high-level visual features with Microsoft cognitive service CV API
         image_name = row[1]
         image_url = 'https://ivc.ischool.utexas.edu/VizWiz/data/Images/%s'%image_name
-        try:
-            ans = analyze_image(image_url, cv_key)
-        except Exception:
-            continue
-        captions = analysis['description']['captions']
-        descriptions = None if len(captions)<1 else captions[0]['text']
-        tags = None if len(analysis['description']['tags'])<1 else analysis['description']['tags']
-        categories = None if len(analysis['categories'])<1 else analysis['categories'][0]['name']
-        adult = analysis['adult']['isAdultContent']
+        image_path = '../data/Images/%s'%image_name
+        
+        captions, descriptions, tags, categories, adult = api_call(image_url, cv_key)
+        resnet_tags = resnet_feature_extract(image_path)
+        vgg_tags = vgg_feature_extract(image_path)
+        
         # aggregate features and class
         temp = {'num_categories':num_categories, 'num_tags': num_tags,
                 'num_colors': num_colors, 'num_faces': num_faces, 
                 'adult': adult, 'categories':categories, 
                 'descriptions':descriptions, 'tags':tags,
+                'resnet_tags':resnet_tags, 'vgg_tags':vgg_tags,
                 'TXT': txt, 'OBJ': obj, 'CNT':cnt, 'OTH':oth}
         features.append(temp)
+        
     return pd.DataFrame(features)
 
+# extract visual feature using ResNet50 trained on ImageNet 
+# https://keras.io/applications/#usage-examples-for-image-classification-models
+# ResNet50 (pre-trained on ImageNet)
+# https://arxiv.org/abs/1512.03385
+def resnet_feature_extract(image_path):
+    model = keras.applications.resnet50.ResNet50(weights='imagenet')
+    img = image.load_img(image_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    preds = model.predict(x)
+    preds_list = decode_predictions(preds, top=3)[0]
+    return [preds_list[i][1] for i in range(len(preds_list))]
+
+
+# Oxford Net / VCG 16 (pre-trained on ImageNet)
+# https://arxiv.org/abs/1409.1556
+def vgg_feature_extract(image_path):
+    model = VGG16(weights='imagenet')
+    img = image.load_img(image_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    preds = model.predict(x)
+    preds_list = decode_predictions(preds, top=3)[0]
+    return [preds_list[i][1] for i in range(len(preds_list))]
+    
+    
+def api_call(image_url, cv_key):
+    try:
+        ans = analyze_image(image_url, cv_key)
+        captions = analysis['description']['captions']
+        descriptions = None if len(captions)<1 else captions[0]['text']
+        tags = None if len(analysis['description']['tags'])<1 else analysis['description']['tags']
+        categories = None if len(analysis['categories'])<1 else analysis['categories'][0]['name']
+        adult = analysis['adult']['isAdultContent']
+    except Exception:
+        captions, descriptions, tags, categories, adult = None, None, None, None, None
+    return captions, descriptions, tags, categories, adult
 
 
 # extract high level visual features from Microsoft cognitive service computer vision API
