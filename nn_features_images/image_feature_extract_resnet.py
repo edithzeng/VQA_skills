e525e89a02a344e2880496c36158660af50c42ea
edithzeng <edith.xiaoyuzeng@gmail.com>
2019-04-07 18:53:29 +0000

    clean up

--- nn_features_images/image_feature_extract_resnet.py
+++ nn_features_images/image_feature_extract_resnet.py
@@ -1,4 +1,4 @@
-""" ResNet trained on ImageNet to extract image feature """
+""" ResNet 50 trained on ImageNet to extract image feature """
 
 import pandas as pd
 import numpy as np
@@ -14,22 +14,12 @@ import sys
 import urllib.error as error
 from pprint import pprint
 import skimage
-from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split, cross_val_score, KFold
-from sklearn.linear_model import LogisticRegression
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import LinearSVC
-from sklearn.ensemble import VotingClassifier, RandomForestClassifier
-from keras.applications.resnet50 import ResNet50
-from keras.applications.resnet import resnet152
+import keras
 from keras.preprocessing import image
-from keras.applications.resnet50 import preprocess_input, decode_predictions
-from keras.applications.vgg16 import VGG16
-from keras.preprocessing import image
-from keras.applications.vgg16 import preprocess_input
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+from keras.applications.inception_v3 import InceptionV3
 import torchvision.models as models
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
@@ -40,57 +30,59 @@ import torchvision.models as models
 from sklearn.metrics import roc_auc_score
 
 def nn_feature_extract_vizwiz(df, logfile):
-    features = []
-    file = open(logfile, "a")
-    file.write("qid,image_feature_vector\n")
-    for i, row in df.iterrows():
-        qid = row[0]
-        image_name = row[1]
-        image_url = 'https://ivc.ischool.utexas.edu/VizWiz/data/Images/%s'%image_name
-        resnet_feature_vector = resnet_feature_extract(image_url=image_url)
-        result = {'qid': qid, 'image_feature_vector': resnet_feature_vector}
-        result_str = "{},{}\n".format(qid, resnet_feature_vector)
-        features.append(result)
-        file.write(result_str)
-    file.close()
-    return pd.DataFrame(features)
-
-def nn_feature_extract_vqa(df, log_file):
 	features = []
 	file = open(logfile, "a")
 	file.write("qid,image_feature_vector\n")
-    for i, row in df.iterrows():
-        qid = row[0]
-        image_name = row[1]
-        resnet_feature_vector = resnet_feature_extract(image_path=image_name)
-        result = {'qid': qid, 'image_feature_vector': resnet_feature_vector}
-        result_str = "{},{}\n".format(qid, resnet_feature_vector)
-        features.append(result)
-        file.write(result_str)
-    file.close()
-    return pd.DataFrame(features)
+	for i, row in df.iterrows():
+		qid = row[0]
+		image_name = row[1]
+		image_url = 'https://ivc.ischool.utexas.edu/VizWiz/data/Images/%s'%image_name
+		nn_feature_vector = resnet_feature_extract(image_url=image_url)
+		result = {'qid': qid, 'image_feature_vector': nn_feature_vector}
+		result_str = "{},{}\n".format(qid, nn_feature_vector)
+		features.append(result)
+		file.write(result_str)
+	file.close()
+	return pd.DataFrame(features)
+
+def nn_feature_extract_vqa(df, logfile):
+	features = []
+	file = open(logfile, "a")
+	file.write("qid,image_feature_vector\n")
+	for i, row in df.iterrows():
+		qid = row[0]
+		image_name = row[1]
+		image_path = "../../VQA_data/images/{}".format(image_name)
+		nn_feature_vector = feature_extract(image_path=image_path)
+		result = {'qid': qid, 'image_feature_vector': nn_feature_vector}
+		result_str = "{},{}\n".format(qid, nn_feature_vector)
+		features.append(result)
+		file.write(result_str)
+	file.close()
+	return pd.DataFrame(features)
 
 # ResNet34: He et al. https://goo.gl/KHQcso
-def resnet_feature_extract(image_url=None, image_path=None):
+# InceptionV3: https://arxiv.org/abs/1512.00567
+def feature_extract(image_url=None, image_path=None):
 	if image_url:
-    	labels_url = 'https://s3.amazonaws.com/outcome-blog/imagenet/labels.json'
-    	response = requests.get(labels_url)
-    	labels = {int(key): value for key, value in response.json().items()}
-    	response = requests.get(image_url)
-    	img = Image.open(io.BytesIO(response.content))
-    if image_path:
-    	img = image.load_image(image_path)
-    transform_pipeline = transforms.Compose([transforms.Resize(224),
-                                             transforms.CenterCrop(224),
-                                             transforms.ToTensor(),
-                                             transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                                  std=[0.229, 0.224, 0.225])])
-    img = transform_pipeline(img)
-    img = img.unsqueeze(0)
-    img = Variable(img)
-    model = models.ResNet152(weights='imagenet')
-    features = model.predict(img)
-    return features
+		labels_url = 'https://s3.amazonaws.com/outcome-blog/imagenet/labels.json'
+		response = requests.get(labels_url)
+		labels = {int(key): value for key, value in response.json().items()}
+		response = requests.get(image_url)
+		img = Image.open(io.BytesIO(response.content))
+	if image_path:
+		img = image.load_img(image_path)
+	transform_pipeline = transforms.Compose([transforms.Resize(224),
+											 transforms.CenterCrop(224),
+											 transforms.ToTensor(),
+											 transforms.Normalize(mean=[0.485, 0.456, 0.406],
+																  std=[0.229, 0.224, 0.225])])
+	img = transform_pipeline(img)
+	img = img.unsqueeze(0)
+	img = Variable(img)
+	model = ResNet50(weights='imagenet')
+	features = model.predict(img)
+	return features
 
 
 
