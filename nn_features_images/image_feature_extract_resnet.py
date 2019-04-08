e9cbba5b5ac7e293bc6fe3446f9a274d001194b9
edithzeng <edith.xiaoyuzeng@gmail.com>
2019-04-08 01:10:41 +0000

    fix bug with tensor

--- nn_features_images/image_feature_extract_resnet.py
+++ nn_features_images/image_feature_extract_resnet.py
@@ -27,6 +27,7 @@ import torchvision.transforms as transforms
 from PIL import Image
 from torch.autograd import Variable
 import torchvision.models as models
+from keras.applications.resnet50 import preprocess_input, decode_predictions
 from sklearn.metrics import roc_auc_score
 
 def nn_feature_extract_vizwiz(df, logfile):
@@ -37,7 +38,7 @@ def nn_feature_extract_vizwiz(df, logfile):
 		qid = row[0]
 		image_name = row[1]
 		image_url = 'https://ivc.ischool.utexas.edu/VizWiz/data/Images/%s'%image_name
-		nn_feature_vector = resnet_feature_extract(image_url=image_url)
+		nn_feature_vector = feature_extract(image_url=image_url)
 		result = {'qid': qid, 'image_feature_vector': nn_feature_vector}
 		result_str = "{},{}\n".format(qid, nn_feature_vector)
 		features.append(result)
@@ -63,6 +64,7 @@ def nn_feature_extract_vqa(df, logfile):
 
 # ResNet34: He et al. https://goo.gl/KHQcso
 # InceptionV3: https://arxiv.org/abs/1512.00567
+# https://keras.io/preprocessing/image/c
 def feature_extract(image_url=None, image_path=None):
 	if image_url:
 		labels_url = 'https://s3.amazonaws.com/outcome-blog/imagenet/labels.json'
@@ -70,16 +72,20 @@ def feature_extract(image_url=None, image_path=None):
 		labels = {int(key): value for key, value in response.json().items()}
 		response = requests.get(image_url)
 		img = Image.open(io.BytesIO(response.content))
-	if image_path:
-		img = image.load_img(image_path)
-	transform_pipeline = transforms.Compose([transforms.Resize(224),
+		transform_pipeline = transforms.Compose([transforms.Resize(224),
 											 transforms.CenterCrop(224),
 											 transforms.ToTensor(),
 											 transforms.Normalize(mean=[0.485, 0.456, 0.406],
 																  std=[0.229, 0.224, 0.225])])
-	img = transform_pipeline(img)
-	img = img.unsqueeze(0)
-	img = Variable(img)
+		img = transform_pipeline(img)
+		img = img.unsqueeze(0)
+		img = Variable(img)
+		img = transforms.ToPILImage()(img)
+	if image_path:
+		img = image.load_img(image_path, target_size=(224,224))
+	img = image.img_to_array(img)
+	img = np.expand_dims(img, axis=0)
+	img = preprocess_input(img)
 	model = ResNet50(weights='imagenet')
 	features = model.predict(img)
 	return features
@@ -88,18 +94,14 @@ def feature_extract(image_url=None, image_path=None):
 
 
 
-# training 
-#vizwiz_train = pd.read_csv("../../vizwiz_skill_typ_train.csv", skipinitialspace=True, engine='python')
-vqa_train = pd.read_csv('../../vqa_skill_typ_train.csv', skipinitialspace=True, engine='python')
+# extract features for VQA training and validation data 
+#vqa_train = pd.read_csv('../../vqa_skill_typ_train.csv', skipinitialspace=True, engine='python')
+#vqa_val = pd.read_csv('../../vqa_skill_typ_val.csv', skipinitialspace=True, engine='python')
+#nn_feature_extract_vqa(vqa_train, "vqa_image_feature_train.csv")
+#nn_feature_extract_vqa(vqa_train, "vqa_image_feature_val.csv")
 
-# validation
-#vizwiz_val = pd.read_csv("../../vizwiz_skill_typ_val.csv", skipinitialspace=True, engine='python')
-vqa_val = pd.read_csv('../../vqa_skill_typ_val.csv', skipinitialspace=True, engine='python')
-
-# label training sets
-#nn_feature_extract_vizwiz(vizwiz_train, "vizwiz_image_feature_train.csv")
-nn_feature_extract_vqa(vqa_train, "vqa_image_feature_train.csv")
-
-# label validation sets
-#nn_feature_extract_vizwiz(vizwiz_train, "vizwiz_image_feature_val.csv")
-nn_feature_extract_vqa(vqa_train, "vqa_image_feature_val.csv")
+# extract image features for VizWiz training and validation data
+vizwiz_train = pd.read_csv("../../vizwiz_skill_typ_train.csv", skipinitialspace=True, engine='python')
+vizwiz_val = pd.read_csv("../../vizwiz_skill_typ_val.csv", skipinitialspace=True, engine='python')
+nn_feature_extract_vizwiz(vizwiz_train, "vizwiz_image_feature_train.csv")
+nn_feature_extract_vizwiz(vizwiz_train, "vizwiz_image_feature_val.csv")
