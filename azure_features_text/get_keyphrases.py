a2fc70fb8fbb331e3d70d819539f8fb00f93fd74
Edith <edith.xiaoyuzeng@gmail.com>
2019-08-18 21:45:19 +0000

    change data path

--- azure_features_text/get_keyphrases.py
+++ azure_features_text/get_keyphrases.py
@@ -11,13 +11,13 @@ key = str(input("Enter text analytics key:"))
 url = 'https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases'
 
 # load data
-vqa_train = pd.read_csv('../../data/vqa_skill_typ_train.csv', skipinitialspace=True, engine='python')
-vqa_val = pd.read_csv('../../data/vqa_skill_typ_val.csv', skipinitialspace=True, engine='python')
-vqa_test = pd.read_csv('../../data/vqa_skill_typ_test.csv', skipinitialspace=True, engine='python')
+vqa_train = pd.read_csv('../../data/three_vote_threshold/vqa_skill_typ_train.csv', skipinitialspace=True, engine='python')
+vqa_val = pd.read_csv('../../data/three_vote_threshold/vqa_skill_typ_val.csv', skipinitialspace=True, engine='python')
+vqa_test = pd.read_csv('../../data/three_vote_threshold/vqa_skill_typ_test.csv', skipinitialspace=True, engine='python')
 
-vizwiz_train = pd.read_csv('../../data/vizwiz_skill_typ_train.csv', skipinitialspace=True, engine='python')
-vizwiz_val = pd.read_csv('../../data/vizwiz_skill_typ_val.csv', skipinitialspace=True, engine='python')
-vizwiz_test = pd.read_csv('../../data/vizwiz_skill_typ_test.csv', skipinitialspace=True, engine='python')
+vizwiz_train = pd.read_csv('../../data/three_vote_threshold/vizwiz_skill_typ_train.csv', skipinitialspace=True, engine='python')
+vizwiz_val = pd.read_csv('../../data/three_vote_threshold/vizwiz_skill_typ_val.csv', skipinitialspace=True, engine='python')
+vizwiz_test = pd.read_csv('../../data/three_vote_threshold/vizwiz_skill_typ_test.csv', skipinitialspace=True, engine='python')
 
 # get key phrases and write to csv files
 get_azure_keyphrases(vqa_train, "vqa_train_keyphrases.csv", key, url)
