44d6532239cce8e1eb97473f6f3a5b5bc1346811
Edith Zeng <edith.xiaoyuzeng@gmail.com>
2019-11-17 21:05:13 -0600

    use local images only for text recognition

--- data/azure_features_images/text_recognition.py
+++ data/azure_features_images/text_recognition.py
@@ -1,24 +1,38 @@
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from util_API_text_recognition import *
+
+parser = argparse.ArgumentParser(description='Procedure to extract relevant features for color recognition label.')
+
+parser.add_argument('--vizwiz_qsn_train', type=str, default='../../data/questions/vizwiz_skill_typ_train.csv')
+parser.add_argument('--vizwiz_qsn_val', type=str, default='../../data/questions/vizwiz_skill_typ_val.csv')
+parser.add_argument('--vizwiz_qsn_test', type=str, default='../../data/questions/vqa_skill_typ_test.csv')
+
+parser.add_argument('--vqa_qsn_train', type=str, default='../../data/questions/vqa_skill_typ_train.csv')
+parser.add_argument('--vqa_qsn_val', type=str, default='../../data/questions/vqa_skill_typ_val.csv')
+parser.add_argument('--vqa_qsn_test', type=str, default='../../data/questions/vqa_skill_typ_test.csv')
+
+args = parser.parse_args
 
 # connection setup - image analysis
 key = str(input("Enter computer vision image analysis key:"))
 vision_base_url = "https://eastus.api.cognitive.microsoft.com/vision/v2.0/"
 
 # training sets
-vizwiz_train = pd.read_csv("../../data/vizwiz_skill_typ_train.csv", skipinitialspace=True, engine='python')
-vqa_train = pd.read_csv('../../data/vqa_skill_typ_train.csv', skipinitialspace=True, engine='python')
-#write_to_file(vision_base_url, key, vqa_train, 'vqa_train_text_recognition.csv', 'vqa')
-
+vizwiz_train = pd.read_csv(args.vizwiz_qsn_train, skipinitialspace=True, engine='python')
+vqa_train = pd.read_csv(args.vqa_qsn_train, skipinitialspace=True, engine='python')
+write_to_file(vision_base_url, key, vqa_train, 'vqa_train_text_recognition.csv', 'vqa')
 write_to_file(vision_base_url, key, vizwiz_train, 'vizwiz_train_text_recognition.csv', 'vizwiz')
 
 # validation sets
-vqa_val = pd.read_csv('../../data/vqa_skill_typ_val.csv', skipinitialspace=True, engine='python')
-vizwiz_val = pd.read_csv("../../data/vizwiz_skill_typ_val.csv", skipinitialspace=True, engine='python')
+vizwiz_val = pd.read_csv(args.vizwiz_qsn_val, skipinitialspace=True, engine='python')
+vqa_val = pd.read_csv(args.vqa_qsn_val, skipinitialspace=True, engine='python')
 write_to_file(vision_base_url, key, vqa_val, 'vqa_val_text_recognition.csv', 'vqa')
 write_to_file(vision_base_url, key, vizwiz_val, 'vizwiz_val_text_recognition.csv', 'vizwiz')
 
 # test sets 
-vqa_test = pd.read_csv('../../data/vqa_skill_typ_test.csv', skipinitialspace=True, engine='python')
-vizwiz_test = pd.read_csv("../../data/vizwiz_skill_typ_test.csv", skipinitialspace=True, engine='python')
+vizwiz_test = pd.read_csv(args.vizwiz_qsn_test, skipinitialspace=True, engine='python')
+vqa_test = pd.read_csv(args.vqa_qsn_test, skipinitialspace=True, engine='python')
 write_to_file(vision_base_url, key, vqa_test, 'vqa_test_text_recognition.csv', 'vqa')
 write_to_file(vision_base_url, key, vizwiz_test, 'vizwiz_test_text_recognition.csv', 'vizwiz')
--- data/azure_features_images/util_API_text_recognition.py
+++ data/azure_features_images/util_API_text_recognition.py
@@ -24,23 +24,21 @@ from PIL import Image
 from io import BytesIO
 
 
-def ocr_request(vision_base_url, key, image_url, local_image=False):
+def ocr_request(vision_base_url, key, image_url):
     """ return a list of lowercase strings, may contain special characters """
     ocr_url = vision_base_url + "ocr"
     params  = {'language': 'en', 'detectOrientation': 'true'}
-    if local_image:
-        image_path = image_url
-        image_data = open(image_path, "rb").read()
-        headers    = {'Ocp-Apim-Subscription-Key': key,
-                      'Content-Type': 'application/octet-stream'}
-        response   = requests.post(ocr_url, headers=headers, params=params, data=image_data)
-    else:
-        data       = {'url': image_url}
-        headers    = {'Ocp-Apim-Subscription-Key': key}
-        response   = requests.post(ocr_url, headers=headers, params=params, json=data)
+
+    image_path = image_url
+    image_data = open(image_path, "rb").read()
+    headers    = {'Ocp-Apim-Subscription-Key': key,
+                  'Content-Type': 'application/octet-stream'}
+    response   = requests.post(ocr_url, headers=headers, params=params, data=image_data)
+
     response.raise_for_status()
     # get response
     analysis = response.json()
+
     # Extract the word bounding boxes and text
     line_infos = [region["lines"] for region in analysis["regions"]]
     word_infos = []
@@ -48,29 +46,28 @@ def ocr_request(vision_base_url, key, image_url, local_image=False):
         for word_metadata in line:
             for word_info in word_metadata["words"]:
                 word_infos.append(word_info)
+
     s = []
     for boundingBox in word_infos:
         s.append(boundingBox['text'].lower())
     return s
 
 # text recognition: similar to OCR but trained with updated recognition models and executes asynchronously
-def recognize_handwritten_text(vision_base_url, key, image_url, local_image=False):
+def recognize_handwritten_text(vision_base_url, key, image_url):
     """ return a list of lowercase strings, may contain special characters """
     """ can take local image path or remote url """
     text_recognition_url = vision_base_url + "recognizeText"
     params  = {'mode': 'Handwritten'}
-    if local_image:
-        image_path = image_url
-        image_data = open(image_path, 'rb').read()
-        headers = {'Ocp-Apim-Subscription-Key': key,
-                   'Content-Type': 'application/octet-stream'}
-        response = requests.post(text_recognition_url, headers=headers, params=params, data=image_data)
-    else:
-        data = {'url': image_url}
-        headers = {'Ocp-Apim-Subscription-Key': key}
-        response = requests.post(text_recognition_url, headers=headers, params=params, json=data)
+
+    image_path = image_url
+    image_data = open(image_path, 'rb').read()
+    headers = {'Ocp-Apim-Subscription-Key': key,
+               'Content-Type': 'application/octet-stream'}
+    response = requests.post(text_recognition_url, headers=headers, params=params, data=image_data)
+    
     response.raise_for_status()
     operation_url = response.headers["Operation-Location"]
+
     # Two API calls: one to submit image for processing, another to retrieve texts found
     analysis = {}
     poll = True      # texts are not immediately available
@@ -82,6 +79,7 @@ def recognize_handwritten_text(vision_base_url, key, image_url, local_image=Fals
             poll = False 
         if ("status" in analysis and analysis['status'] == 'Failed'):
             poll = False
+    
     polygons = []
     if ("recognitionResult" in analysis):
         polygons = [(line["boundingBox"], line["text"])
@@ -92,17 +90,17 @@ def recognize_handwritten_text(vision_base_url, key, image_url, local_image=Fals
     return s
 
 
-def write_to_file(vision_base_url, key, df, output_file_path, dataset): 
+
+def write_to_file(vision_base_url, key, df, output_file_path, dataset):
+
     if (not (isinstance(df,pd.DataFrame) and dataset in ['vizwiz','vqa'])):
-        raise ValueError("Check arguments")
+        raise ValueError
     file = open(output_file_path, 'w+')
     file.write("qid;question;ocr_text;handwritten_text\n")
-    local_image = False
-    image_url_base = 'https://ivc.ischool.utexas.edu/VizWiz/data/Images/'
-    if dataset == 'vqa':
-        local_image = True
-        image_url_base = os.path.abspath('../../VQA_data/image/')
+
+    image_url_base = os.path.abspath(f'../../VQA_data/image/{dataset}')
     assert(len(df) > 0)
+    
     n = 1
     for i, row in df.iterrows():
         if (n%100 == 0):
@@ -112,13 +110,14 @@ def write_to_file(vision_base_url, key, df, output_file_path, dataset):
         qsn = row[df.columns.get_loc('QSN')]
         image_url = os.path.join(image_url_base, img)
         try:
-            ocr_text   = ocr_request(vision_base_url, key, image_url, local_image)
-            handwritten_text = recognize_handwritten_text(vision_base_url, key, image_url, local_image)
-            result_str = "{};{};{};{}\n".format(qid,qsn,ocr_text,handwritten_text)
+            ocr_text   = ocr_request(vision_base_url, key, image_url)
+            handwritten_text = recognize_handwritten_text(vision_base_url, key, image_url)
+            result_str = "{};{};{};{}\n".format(qid, qsn, ocr_text, handwritten_text)
             file.write(result_str)
-        except requests.exceptions.HTTPError as e:     # 400 error
+        except requests.exceptions.HTTPError as e:
             print(e, file=sys.stderr)
             continue
         n += 1
+
     file.close()
     print("OCR and handwritten text recognition results for {} written to {}".format(dataset, output_file_path))
