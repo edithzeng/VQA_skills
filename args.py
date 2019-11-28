c7e7812f62bce8a028869a87030e0f6234b8daed
edithzeng <edith.xiaoyuzeng@gmail.com>
2019-11-28 21:21:36 +0000

    get previously extracted features

--- args.py
+++ args.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 # paths
 DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 
@@ -13,3 +14,23 @@ vqa_qsn_test = os.path.join(DATA_DIR, 'vqa_skill_typ_test.csv')
 
 vizwiz_sets = [vizwiz_qsn_train, vizwiz_qsn_val, vizwiz_qsn_test]
 vqa_sets = [vqa_qsn_train, vqa_qsn_val, vqa_qsn_test]
+
+# image-based features
+IMG_X = os.path.join(DATA_DIR, 'azure_features_images')
+vizwiz_color_recog_train = os.path.join(IMG_X, 'vizwiz_train_color_recognition.csv')
+vizwiz_color_recog_val = os.path.join(IMG_X, 'vizwiz_val_color_recognition.csv')
+vizwiz_color_recog_test = os.path.join(IMG_X, 'vizwiz_test_color_recognition.csv')
+
+vizwiz_text_recog_train = os.path.join(IMG_X, 'vizwiz_train_text_recognition.csv') 
+vizwiz_text_recog_val = os.path.join(IMG_X, 'vizwiz_val_text_recognition.csv')
+vizwiz_text_recog_test = os.path.join(IMG_X, 'vizwiz_test_text_recognition.csv') 
+
+vqa_color_recog_train = os.path.join(IMG_X, 'vqa_train_color_recognition.csv') 
+vqa_color_recog_val = os.path.join(IMG_X, 'vqa_val_color_recognition.csv') 
+vqa_color_recog_test = os.path.join(IMG_X,'vqa_test_color_recognition.csv') 
+    
+vqa_text_recog_train = os.path.join(IMG_X, 'vqa_train_text_recognition.csv')
+vqa_text_recog_val = os.path.join(IMG_X, 'vqa_val_text_recognition.csv') 
+vqa_text_recognition_test = os.path.join(IMG_X, 'vqa_test_text_recognition.csv')
+
+# question-based features
